4e0e753929812040e466636ebb9473f9a961373c
ddjefferson <ddjefferson@yahoo.com>
2023-01-09 17:16:36 +0000

    Learning about functions

--- challenges/lab67.py
+++ challenges/lab67.py
@@ -4,15 +4,15 @@ Looping Vampires"""
 
 counter = 0
 ## create file object in "r"ead mode
-with open("dracula.txt", "r") as foo:
+with open("dracula.txt", "r") as dracula:
     ## create second file in "w"rite mode
     with open("vampytimex.txt", "w") as fang:
     ## loop through line that contains word vampire
-        for line in foo:
+        for line in dracula:
             if "vampire" in line.lower():
                 print(line)
                 counter += 1
                 fang.write(line)
 
-print(counter)
+print("Total number of lines witht the word vampire:", counter)
 
